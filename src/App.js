3b26b1246742ebb2abd865df911093e86b7813fd
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-05-12 17:38:36 +0900

    navbar and border

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./style/main.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="cool-border" />
+        <div className="top">
+          <div className="social-links">
+            <a href="#">
+              <i className="fab fa-facebook-f" />
+            </a>
+            <a href="#">
+              <i className="fab fa-youtube" />
+            </a>
+            <a href="#">
+              <i className="fab fa-instagram" />
+            </a>
+            <a href="#">
+              <i className="fab fa-twitter" />
+            </a>
+            <a href="#">
+              <i className="fab fa-google-plus-g" />
+            </a>
+          </div>
+          <div className="navigation-column">
+            <a href="#">
+              <p>Tours</p>
+            </a>
+            <a href="#">
+              <p>Sites</p>
+            </a>
+            <a href="#">
+              <p>Peter Macintosh</p>
+            </a>
+            <a href="#">
+              <p>Gallery</p>
+            </a>
+            <a href="#">
+              <p>VIP Services</p>
+            </a>
+            <a href="#">
+              <p>Shop</p>
+            </a>
+            <a href="#">
+              <p>Walking Lecture</p>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
