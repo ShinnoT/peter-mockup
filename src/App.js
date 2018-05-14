97f52604883a6216f98fd468b8cfcbdcb8e5c757
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-05-14 20:53:29 +0900

    design complete with footer and navbar

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ class App extends Component {
     return (
       <div className="App">
         <div className="navbar">
-          <div className="left-content">title logo</div>
-          <div className="right-content">list of tabs to click</div>
+          <div className="left-content">KSN</div>
+          <div className="right-content">
+            <a href="#">Walking Lecture</a>
+            <a href="#">Peter Macintosh</a>
+            <a href="#">VIP Tours</a>
+          </div>
         </div>
         <div className="banner">
           <h1>
             Kyoto <strong>Sights</strong> & <strong>Nights</strong>
           </h1>
         </div>
-        <div className="footer">some footer here</div>
+        <div className="footer">
+          <i className="fab fa-twitter" />
+          <i className="fab fa-facebook-f" />
+          <i className="fab fa-youtube" />
+          <i className="fab fa-instagram" />
+          <i className="fab fa-google-plus-g" />
+        </div>
       </div>
     );
   }
