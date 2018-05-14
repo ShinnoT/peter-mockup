b7fd1d98411b79a33460900789595cc4694b2221
Shinnosuke Taguchi <shinno.ma30@gmail.com>
2018-05-14 22:10:15 +0900

    finished

--- src/App.js
+++ src/App.js
@@ -6,13 +6,37 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="side-bar">
+          <div className="top-content">
+            <p>KSN</p>
+            <i className="fas fa-ellipsis-h" />
+          </div>
+          <div className="bottom-content">
+            <i class="fab fa-facebook-f" />
+            <i class="fab fa-twitter" />
+            <i class="fab fa-instagram" />
+            <i class="fab fa-google-plus-g" />
+          </div>
+        </div>
+        <div className="main-content">
+          <div className="top-pics">
+            <div className="first-image">
+              <div className="inner">
+                <p>VIP Tours</p>
+              </div>
+            </div>
+            <div className="second-image">
+              <div className="inner">
+                <p>Walking Lecture</p>
+              </div>
+            </div>
+          </div>
+          <div className="bottom-pics">
+            <div className="banner">
+              <p>Peter Macintosh</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
